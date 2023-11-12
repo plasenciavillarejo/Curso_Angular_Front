7aeff960df52c2e282c60b8b7ae4f457cef6b117
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2023-11-12 20:40:49 +0100

    Subida Login Front

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2'; 
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
  
   titulo:String = 'Por favor inicie sesión!';
+  usuario:Usuario;
 
   constructor() {
-
+    // Inicializamos el usuario
+    this.usuario = new Usuario();
   }
 
   ngOnInit(): void {
  
   }
+
+  login():void {
+    console.log("El usuario que accede es: " + this.usuario.username + " " + this.usuario.password);
+    if(this.usuario.username == null || this.usuario.password == null) {
+      Swal.fire('Error login', 'Username o Password vacías!', 'error');
+      return;
+    }
+  }
+
 
 }
