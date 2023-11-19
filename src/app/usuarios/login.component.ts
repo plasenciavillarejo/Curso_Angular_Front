bb12beb9c199c7c5c6fac2fbddff5b5c6e8b4cbd
Jose Plasencia Villarejo <plasenciavillarejo@gmail.com>
2023-11-19 19:47:50 +0100

    Subida login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Usuario } from './usuario';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
@@ -7,13 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
  
   titulo:String = 'Por favor inicie sesión!';
+  usuario: Usuario;
 
   constructor() {
-
+    this.usuario = new Usuario();
   }
 
   ngOnInit(): void {
  
+  }
+
+  login() : void {
+    console.log(this.usuario);
+
+    if(this.usuario.username == null || this.usuario.password == null) {
+      Swal.fire('Error Login', 'Username o password vacías!', 'error');
+      return;
+    }
+
   }
 
 }
