507090d4dabd7ff841b8056404650bf698ff203d
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2023-12-24 16:03:36 +0100

    Correción en el login incial de Angular - Funciona correctamente

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -16,22 +16,26 @@ export class AuthService {
   // Retorna todos los datos que contiene el login. Token, Usuario, etc...
   login(usuario: Usuario):Observable<any> {
     const urlEndpoint = 'http://localhost:8090/api/security/oauth/token';
+    
     // Las credenciales de el front, pero debemos encriptarlas en base64 con btoa()
-    const credenciales = btoa('angularapp' + ':' + '12345');
-   
+    //const credenciales = btoa('frontendapp' + ':' + '12345');
+    const credenciales = btoa('frontendapp' + ':' + '12345');
+
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization': 'Basic ' + credenciales
     });
     
     let params = new URLSearchParams();
-    params.set('grant_type','password');
+    params.set('grant_type', 'password');
     params.set('username', usuario.username);
     params.set('password', usuario.password);
 
     console.log('Parametros generados: ' + params.toString());
     // params.toString() para convertir los parámetros en una cadena de string -> gran_type=password&username=jose&password=12345
-    return this.http.post<any>(urlEndpoint, params.toString(), {headers: httpHeaders});
+    return this.http.post<any>(urlEndpoint, params.toString(), {headers: 
+      httpHeaders
+    });
   }
 
 }
