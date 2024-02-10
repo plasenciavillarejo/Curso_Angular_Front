871b65a6c36eb0f541ea32ef1d162ea06f43214d
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-02-10 18:54:28 +0100

    Clase 180 - Terminada

--- src/app/facturas/services/factura.service.ts
+++ src/app/facturas/services/factura.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of, pipe, throwError } from 'rxjs';
 import { AuthService } from 'src/app/usuarios/auth.service';
 import { Factura } from '../models/factura';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ import { Factura } from '../models/factura';
 export class FacturasService implements OnInit {
 
   private urlEndpointFacturas: string = 'http://localhost:8090/api/productos/listar/factura';
-  
+  private urlEndpointBorrarFacturas: string = 'http://localhost:8090/api/productos/facturas';
   // Cabeceras http
   private httpHeaders = new HttpHeaders({
     'Content-type': 'application/json'
@@ -31,5 +32,18 @@ export class FacturasService implements OnInit {
     return this.http.get<Factura>(`${this.urlEndpointFacturas}/${id}`);
   }
 
-  
+  borrarFactura(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.urlEndpointBorrarFacturas}/${id}`).pipe(
+      catchError(errorCapturadoDesdeBE => {
+        console.log(errorCapturadoDesdeBE.error.mensaje)
+        Swal.fire('Error al borrar una factura', errorCapturadoDesdeBE.error.mensaje, 'error');
+        return throwError(
+          () => errorCapturadoDesdeBE
+        );
+      })
+    );
+
+  }
+
+
 }
